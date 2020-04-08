c44345067f6e4a9c2c0ee6d6354975f6677581fa
jsferrer1 <jsferrer1@yahoo.com>
2020-04-08 09:27:36 +0800

    Initial WelcomeDataService

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,13 +12,29 @@ export class WelcomeComponent implements OnInit {
   message = 'Welcome message'
   name = ''
 
-  constructor(private route:ActivatedRoute) {
+  constructor(
+    private route:ActivatedRoute,
+    private service: WelcomeDataService) {
 
   }
 
   ngOnInit() {
     //console.log(this.message)
     this.name = this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe();
+
+    // this.service.executeHelloWorldBeanService().subscribe(
+    //   response => this.handleSuccessfulResponse(response),
+    //   error => this.handleErrorResponse(error)
+    // );
+
+    //console.log('last line of getWelcomeMessage')
+
+    //console.log("get welcome message");
   }
 
 }
